d8e21dacbed0e7a1fa0bcb262a2841ad57bcf517
Avantik-Tomar <avantiktomar2@gmail.com>
2024-10-07 08:01:53 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+namespace ExcelProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new Excel package
+            var fileInfo = new FileInfo("SampleExcel.xlsx");
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                // Add a new worksheet to the empty workbook
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                // Add some sample data
+                worksheet.Cells[1, 1].Value = "Hello, World!";
+                worksheet.Cells[2, 1].Value = "This is an Excel file created with EPPlus.";
+
+                // Save the Excel file
+                package.Save();
+            }
+
+            Console.WriteLine("Excel file created successfully!");
+        }
+    }
+}
